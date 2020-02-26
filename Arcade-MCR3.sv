5e7a4d64a3c85a64f41db2d7d784d3d5bc7aabcf
NeuroRulez <neuro_999@hotmail.com>
2020-02-26 21:59:53 +0100

    DB15 OSD and release

--- Arcade-MCR3.sv
+++ Arcade-MCR3.sv
@@ -110,6 +110,7 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
+    output  	  USER_OSD,	
     output	      USER_MODE,	
 	input   [6:0] USER_IN,
 	output  [6:0] USER_OUT
@@ -121,6 +122,7 @@ wire   JOY_CLK, JOY_LOAD;
 wire   JOY_DATA  = USER_IN[5];
 assign USER_OUT  = |status[31:30] ? {5'b11111,JOY_CLK,JOY_LOAD} : '1;
 assign USER_MODE = |status[31:30] ;
+assign USER_OSD  = joydb15_1[8] & joydb15_1[6];
 
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
@@ -220,6 +222,7 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ioctl_index(ioctl_index),
 	.ioctl_wait(ioctl_wait),
 
+	.joy_raw(joydb15_1[5:0]),
 	.joystick_0(joy1_USB),
 	.joystick_1(joy2_USB),
 
